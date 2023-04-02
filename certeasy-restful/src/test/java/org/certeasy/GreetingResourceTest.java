84b5f25333ae9092b9ff01f42dd919dfc5c14114
Mário Júnior <francisco.junior.mario@gmail.com>
2023-04-02 19:25:58 +0200

    fix: certeasy-restful unit tests

--- certeasy-restful/src/test/java/org/certeasy/GreetingResourceTest.java
+++ certeasy-restful/src/test/java/org/certeasy/GreetingResourceTest.java
@@ -14,8 +14,7 @@ public class GreetingResourceTest {
         given()
           .when().get("/hello")
           .then()
-             .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+             .statusCode(404);
     }
 
 }
