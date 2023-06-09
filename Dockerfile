0032affbb0957dd1aed4c631b2ea957ce1f9bd94
sambd001 <dsambo.sambo@vm.co.mz>
2023-06-09 23:18:47 +0200

    fix: uses the --chown flag to set the ownership of the *-runner

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM quay.io/quarkus/quarkus-micro-image:2.0
 WORKDIR /work/
-COPY *-runner /work/application
-RUN chmod 775 /work
+COPY --chown=quarkus:quarkus *-runner /work/application
+RUN chmod 775 /work/application
 EXPOSE 8080
 CMD ["./application", "-Dquarkus.http.host=0.0.0.0"]
--- certeasy-api-tests/src/tests/test_create_issuer_from_spec/test_create_issuer_from_spec.py
+++ certeasy-api-tests/src/tests/test_create_issuer_from_spec/test_create_issuer_from_spec.py
@@ -3,7 +3,6 @@ import requests
 import time
 import pytest
 
-
 @pytest.fixture(scope="module")
 def app_container():
     # Create a Docker client
@@ -16,6 +15,7 @@ def app_container():
     # Start the container
     container = client.containers.run(image_name, detach=True, ports={f'{container_port}/tcp': container_port})
     print(container)
+
     # Wait for the container to start
     time.sleep(2)
 
@@ -25,7 +25,6 @@ def app_container():
     container.stop()
     container.remove()
 
-
 def test_api_status(app_container):
     # Make a request to the API
     url = f'http://localhost:8000/'
@@ -34,5 +33,3 @@ def test_api_status(app_container):
     # Assert the response status
     expected_status = 200
     assert response.status_code == expected_status, f'Expected status {expected_status}, but received {response.status_code}'
-
-    # Add more assertions as needed
