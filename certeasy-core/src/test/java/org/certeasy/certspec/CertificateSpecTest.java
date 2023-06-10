99d5c3ff5d29cba00ca8dd1a9999f1fada8cadf9
Mario Junior <francisco.junior.mario@gmail.com>
2023-06-10 13:26:42 +0200

    wip: DateRange tests

---
+++ certeasy-core/src/test/java/org/certeasy/certspec/CertificateSpecTest.java
@@ -0,0 +1,46 @@
+package org.certeasy.certspec;
+
+import org.junit.jupiter.api.Test;
+
+public class CertificateSpecTest {
+
+
+    @Test
+    public void constructorMustAllowNullExtendedKeyUsage(){
+
+    }
+
+
+    @Test
+    public void mustReturnProvidedSubject(){
+
+    }
+
+    @Test
+    public void mustReturnProvidedKeyStrength(){
+
+    }
+
+    @Test
+    public void mustReturnProvidedValidityPeriod(){
+
+    }
+
+    @Test
+    public void mustReturnProvidedBasicConstraints(){
+
+    }
+
+    @Test
+    public void mustReturnProvidedKeyUsage(){
+
+    }
+
+    @Test
+    public void mustReturnProvidedExtendedKeyUsage(){
+
+    }
+
+
+
+}
