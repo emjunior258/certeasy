28aaf70e4dfdb69d38f9b93d1fcedc122aea7011
Mario Junior <francisco.junior.mario@gmail.com>
2023-06-10 14:25:52 +0200

    wip: tests for CertificateSubject

---
+++ certeasy-core/src/test/java/org/certeasy/CertificateSubjectTest.java
@@ -0,0 +1,104 @@
+package org.certeasy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CertificateSubjectTest {
+
+    @Test
+    @DisplayName("constructor must succeed with null alternative names")
+    public void constructorMustSucceedWithNullAlternativeNames(){
+        new CertificateSubject(
+                DistinguishedName.builder()
+                        .parse("CN=John Wick, C=MZ")
+                        .build()
+                , null);
+    }
+
+    @Test
+    @DisplayName("constructor must not succeed with null DistinguishedName")
+    public void constructorMustNotSucceedWithNullDistinguishedName(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            new CertificateSubject(
+                    null,
+                    Set.of(
+                            new SubjectAlternativeName(SubjectAlternativeNameType.DNS, "test.example.com"),
+                            new SubjectAlternativeName(SubjectAlternativeNameType.DNS, "demo.example.com")
+                    ));
+        });
+    }
+
+
+    @Test
+    @DisplayName("setDistinguishedName() must change distinguishedName")
+    public void setDistinguishedNameMustChangeDistinguishedName(){
+       CertificateSubject subject =  new CertificateSubject(
+                DistinguishedName.builder()
+                        .parse("CN=John Wick, C=UK")
+                        .build());
+       subject.setDistinguishedName(
+               DistinguishedName.builder()
+                       .parse("CN=John Doe, C=US")
+                       .build());
+       DistinguishedName distinguishedName = subject.getDistinguishedName();
+       assertEquals("John Doe", distinguishedName.getCommonName());
+       Optional<RelativeDistinguishedName> countryName = distinguishedName.findFirst(SubjectAttributeType.CountryName);
+       assertTrue(countryName.isPresent());
+       assertEquals("US", countryName.get().value());
+    }
+
+
+    @Test
+    @DisplayName("setAlternativeNames() must change alternative names")
+    public void setAlternativeNamesMustSubjectChangeAlternativeNames(){
+        CertificateSubject subject =  new CertificateSubject(
+                DistinguishedName.builder()
+                        .parse("CN=John Wick, C=UK")
+                        .build());
+        subject.setAlternativeNames(Set.of(
+                new SubjectAlternativeName(SubjectAlternativeNameType.OTHER_NAME, "Baga Yaga"),
+                new SubjectAlternativeName(SubjectAlternativeNameType.DNS, "johnwick.com")
+        ));
+    }
+
+    @Test
+    @DisplayName("getCommonName() must return DistinguishedName CN attribute")
+    public void getCommonNameMustReturnDistinguishedNameCN(){
+        CertificateSubject subject =  new CertificateSubject(
+                DistinguishedName.builder()
+                        .parse("CN=John Wick, C=UK")
+                        .build());
+        assertEquals("John Wick", subject.getCommonName());
+    }
+
+    @Test
+    @DisplayName("hasAlternativeNames() must return false if alternative names are not set")
+    public void hasAlternativeNamesMustReturnFalseIfAlternativeNamesAreNotSet(){
+        CertificateSubject subject =  new CertificateSubject(
+                DistinguishedName.builder()
+                        .parse("CN=John Wick, C=UK")
+                        .build());
+        assertFalse(subject.hasAlternativeNames());
+    }
+
+    @Test
+    @DisplayName("hasAlternativeNames() must return true if alternative names are set")
+    public void hasAlternativeNamesMustReturnTrueIfAlternativeNamesAreSet(){
+        CertificateSubject subject =  new CertificateSubject(
+                DistinguishedName.builder()
+                        .parse("CN=John Wick, C=UK")
+                        .build());
+        subject.setAlternativeNames(Set.of(
+                new SubjectAlternativeName(SubjectAlternativeNameType.OTHER_NAME, "Baga Yaga"),
+                new SubjectAlternativeName(SubjectAlternativeNameType.DNS, "johnwick.com")
+        ));
+        assertTrue(subject.hasAlternativeNames());
+    }
+
+
+}
