ea9d3301cbb504b31f2596d77b54f71ca1b33c71
sambd001 <dsambo.sambo@vm.co.mz>
2023-06-15 16:26:11 +0200

    feat: add create issuer from spec tests

--- certeasy_api_tests/src/tests/test_create_issuer_from_spec/test_create_issuer_from_spec.py
+++ certeasy_api_tests/src/tests/test_create_issuer_from_spec/test_create_issuer_from_spec.py
@@ -58,3 +58,286 @@ def test_should_not_allows_creation_of_issuer_with_empty_body(app_container):
         response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
         response_status = response.status_code
     assert response_status == 409
+
+
+def test_should_not_allows_creation_of_issuer_with_empty_name(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(filename='../services/data/create_issuer/specs/invalid_body/name/empty_name.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_sequence_of_space_name(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(
+        filename='../services/data/create_issuer/specs/invalid_body/name/sequence_of_space_name.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_null_name(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(filename='../services/data/create_issuer/specs/invalid_body/name/null_name.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_empty_strength(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(
+        filename='../services/data/create_issuer/specs/invalid_body/key_strength/empty_key_strength.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_sequence_of_space_strength(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(
+        filename='../services/data/create_issuer/specs/invalid_body/key_strength/sequence_of_space_key_strength.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_null_strength(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(
+        filename='../services/data/create_issuer/specs/invalid_body/key_strength/null_key_strength.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_empty_validity_object(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(filename='../services/data/create_issuer/specs/invalid_body/validity/empty_validity.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_null_validity_object(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(filename='../services/data/create_issuer/specs/invalid_body/validity/null_validity.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_empty_from_date(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(filename='../services/data/create_issuer/specs/invalid_body/validity/empty_from_date.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_sequence_of_space_from_date(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(
+        filename='../services/data/create_issuer/specs/invalid_body/validity/sequence_of_space_from_date.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_null_from_date(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(filename='../services/data/create_issuer/specs/invalid_body/validity/null_from_date.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_empty_until_date(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(
+        filename='../services/data/create_issuer/specs/invalid_body/validity/empty_until_date.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_sequence_of_space_until_date(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(
+        filename='../services/data/create_issuer/specs/invalid_body/validity/sequence_of_space_until_date.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_null_until_date(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(filename='../services/data/create_issuer/specs/invalid_body/validity/null_until_date.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_invalid_until_date(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(
+        filename='../services/data/create_issuer/specs/invalid_body/validity/invalid_until_date.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_invalid_from_date(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(
+        filename='../services/data/create_issuer/specs/invalid_body/validity/invalid_from_date.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_empty_address_object(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(filename='../services/data/create_issuer/specs/invalid_body/address/empty_address.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_null_address_object(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(filename='../services/data/create_issuer/specs/invalid_body/address/null_address.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_empty_country(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(filename='../services/data/create_issuer/specs/invalid_body/address/empty_country.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_sequence_of_space_country(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(
+        filename='../services/data/create_issuer/specs/invalid_body/address/sequence_of_space_country.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_null_country(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(filename='../services/data/create_issuer/specs/invalid_body/address/empty_country.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_empty_state(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(filename='../services/data/create_issuer/specs/invalid_body/address/empty_state.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_sequence_of_space_state(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(
+        filename='../services/data/create_issuer/specs/invalid_body/address/sequence_of_space_state.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_null_state(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(filename='../services/data/create_issuer/specs/invalid_body/address/null_state.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_empty_locality(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(filename='../services/data/create_issuer/specs/invalid_body/address/empty_locality.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_sequence_of_space_locality(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(
+        filename='../services/data/create_issuer/specs/invalid_body/address/sequence_of_space_locality.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_null_locality(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(filename='../services/data/create_issuer/specs/invalid_body/address/null_locality.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_empty_street_address(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(filename='../services/data/create_issuer/specs/invalid_body/address/empty_street_address.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_sequence_of_space_street_address(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(
+        filename='../services/data/create_issuer/specs/invalid_body/address/sequence_of_space_address.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
+
+
+def test_should_not_allows_creation_of_issuer_with_null_street_address(app_container):
+    ISSUER_ID = generate_issuer_id()
+    VALID_BODY = file_reader(filename='../services/data/create_issuer/specs/invalid_body/address/null_street_address.json')
+
+    # Make a request to the API
+    response = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID}/cert-spec', json=VALID_BODY)
+    assert response.status_code == 422
