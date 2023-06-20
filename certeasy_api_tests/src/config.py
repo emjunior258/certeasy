42b9690eae5dcbf9c45b1df55b9d70b85374f54c
sambd001 <dsambo.sambo@vm.co.mz>
2023-06-20 19:44:20 +0200

    fix: try to get image env variable

--- certeasy_api_tests/src/config.py
+++ certeasy_api_tests/src/config.py
@@ -1,4 +1,5 @@
 import os
+
 DOCKER_IMAGE_PORT = '8080'
-DOCKER_IMAGE_NAME = os.getenv('DOCKER_IMAGE_NAME')
-BASE_URL = "http://0.0.0.0:8080/api/"
+DOCKER_IMAGE_NAME = os.getenv('DOCKER_IMAGE_NAME', 'ghcr.io/certeasy:test')
+BASE_URL = f"http://0.0.0.0:{DOCKER_IMAGE_PORT}/api/"
