643d00ea3fed5ebff6e7832bbbd22cd3ccdb7ee2
Stelio Sergio <nsteliosergio@gmail.com>
2023-06-26 10:23:28 +0200

    chore(ci): pass generated image name to tests

--- certeasy_api_tests/src/config.py
+++ certeasy_api_tests/src/config.py
@@ -1,4 +1,4 @@
 DOCKER_IMAGE_PORT = '8080'
-DOCKER_IMAGE_NAME = 'ghcr.io/certeasy:test'
+# DOCKER_IMAGE_NAME = 'ghcr.io/certeasy:test'
 # DOCKER_IMAGE_NAME = 'ghcr.io/emjunior258/certeasy:develop'
 BASE_URL = "http://0.0.0.0:8080/api/"
