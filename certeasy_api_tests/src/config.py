2ecc15e82f83fff8753c47f7921613e5d2771a01
sambd001 <dsambo.sambo@vm.co.mz>
2023-07-01 14:40:12 +0200

    add docker env variable

--- certeasy_api_tests/src/config.py
+++ certeasy_api_tests/src/config.py
@@ -1,6 +1,5 @@
 import os
 
 DOCKER_IMAGE_PORT = '8080'
-# DOCKER_IMAGE_NAME = os.getenv('DOCKER_IMAGE_NAME', 'ghcr.io/certeasy:test')
 DOCKER_IMAGE_NAME = os.getenv('DOCKER_IMAGE_NAME')
 BASE_URL = f"http://0.0.0.0:{DOCKER_IMAGE_PORT}/api/"
