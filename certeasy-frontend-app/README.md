6b2c0c7b87fc3f19e46c9113fdc623653283912c
Marino Ricardo <74591655+marinoricardo@users.noreply.github.com>
2023-07-15 17:56:25 +0200

    setup project and boilerplate

--- certeasy-frontend-app/README.md
+++ certeasy-frontend-app/README.md
@@ -15,6 +15,11 @@ npm run serve
 npm run build
 ```
 
+### Run your end-to-end tests
+```
+npm run test:e2e
+```
+
 ### Lints and fixes files
 ```
 npm run lint
--- certeasy-frontend-app/src/components/HelloWorld.vue
+++ certeasy-frontend-app/src/components/HelloWorld.vue
@@ -9,7 +9,11 @@
     <h3>Installed CLI Plugins</h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-e2e-cypress" target="_blank" rel="noopener">e2e-cypress</a></li>
     </ul>
     <h3>Essential Links</h3>
     <ul>
