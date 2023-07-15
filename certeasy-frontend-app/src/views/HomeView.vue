359a23c98c03280070e92480439e8e95bde0dd56
Felizardo Ntswambeni Massimbe <f2massimbe@gmail.com>
2023-07-15 18:53:47 +0100

    feat: clean up boilerplate

--- certeasy-frontend-app/src/views/HomeView.vue
+++ certeasy-frontend-app/src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Cearteasy" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
