ad2ae6aa8d4b05f25e871a7b03e5d442a5366f77
Stelio Sergio <nsteliosergio@gmail.com>
2023-07-16 11:36:14 +0200

    chore: create test data dir in the test profile

--- certeasy-backend-app/src/test/java/org/certeasy/backend/persistence/DirectoryPersistenceProfile.java
+++ certeasy-backend-app/src/test/java/org/certeasy/backend/persistence/DirectoryPersistenceProfile.java
@@ -3,9 +3,30 @@ package org.certeasy.backend.persistence;
 import io.quarkus.test.junit.QuarkusTestProfile;
 import org.certeasy.backend.CertConstants;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
 import java.util.Map;
+import java.util.Set;
 
 public class DirectoryPersistenceProfile implements QuarkusTestProfile {
+
+    static {
+        Path path = DirectoryBaseTest.DATA_DIRECTORY; //Path.of("target","test");
+        if (Files.notExists(path)) {
+            Set<PosixFilePermission> permissions = PosixFilePermissions.fromString("rwxr--r--");
+            FileAttribute<Set<PosixFilePermission>> fileAttributes = PosixFilePermissions.asFileAttribute(permissions);
+            try {
+                System.out.println("Creating directory {"+ path.toAbsolutePath() + "}");
+                Files.createDirectories(path, fileAttributes);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
     @Override
     public Map<String, String> getConfigOverrides(){
