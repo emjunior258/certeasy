3e7f21c3bdf873011dd02c6d36127007f2514c1e
Felizardo Ntswambeni Massimbe <f2massimbe@gmail.com>
2023-07-30 01:28:57 +0100

    style: setup tailwind css

--- certeasy-console-app/src/main.js
+++ certeasy-console-app/src/main.js
@@ -1,7 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import "./index.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).mount("#app");
