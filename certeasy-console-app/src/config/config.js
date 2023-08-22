045be3b7f87632652b6904081ddc4e931f5a7621
Felizardo Ntswambeni Massimbe <f2massimbe@gmail.com>
2023-08-22 14:10:38 +0100

    feat: setup mock service worker

--- certeasy-console-app/src/config/config.js
+++ certeasy-console-app/src/config/config.js
@@ -1,7 +1,7 @@
-import axios from 'axios'
-
+import axios from "axios";
+export const SERVER = "http://localhost:8080/api";
 const api = axios.create({
-    baseURL : 'http://localhost:8080/api'
+  baseURL: SERVER,
 });
 
 export default api;
