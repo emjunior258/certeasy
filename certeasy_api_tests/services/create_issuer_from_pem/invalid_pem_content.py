1a694fbcea57625e49f6e9f9fac3a313c8c267c9
sambd001 <dsambo.sambo@vm.co.mz>
2023-08-27 16:30:59 +0200

    fix: use if condition to upload test report even when test fails

--- certeasy_api_tests/services/create_issuer_from_pem/invalid_pem_content.py
+++ certeasy_api_tests/services/create_issuer_from_pem/invalid_pem_content.py
@@ -1,2 +1,6 @@
 incomplete_cert_pem = "-----BEGIN CERTIFICATE-----\n" \
                       "MIICyjCCAbKgAwIBAgIUFufrz//pKZwz6Dqyv4U20gUHWZYwDQYJKoZIhvcNAQEL\n"
+
+incomplete_key_pem = "-----BEGIN PRIVATE KEY-----\n" \
+                     "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDMmQlryOcq8A48\n" \
+                     "J8XE9/k0PTU2O2RfIQEtatpJnTG8IZXSF1w==\n"
