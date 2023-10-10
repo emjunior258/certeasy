987ae2af570c8a0c672e28f8f71ce1b267a202e9
Stelio Sergio <nsteliosergio@gmail.com>
2023-10-10 18:58:34 +0200

    fix: associate child ca to parent ca (#112)

--- certeasy-backend-app/src/main/java/org/certeasy/backend/issuer/IssuerParent.java
+++ certeasy-backend-app/src/main/java/org/certeasy/backend/issuer/IssuerParent.java
@@ -1,8 +1,10 @@
 package org.certeasy.backend.issuer;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import io.quarkus.runtime.annotations.RegisterForReflection;
 
 @JsonPropertyOrder({"id", "name"})
+@RegisterForReflection
 public record IssuerParent(String id, String name) {
 
 }
