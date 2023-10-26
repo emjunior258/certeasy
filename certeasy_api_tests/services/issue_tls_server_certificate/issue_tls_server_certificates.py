4e39e5c11ea19ca2c2cd212bfc90087eb7720821
sambd001 <dsambo.sambo@vm.co.mz>
2023-10-26 20:01:14 +0200

    fix: test_list_issued_tls_server_certificate.py

--- certeasy_api_tests/services/issue_tls_server_certificate/issue_tls_server_certificates.py
+++ certeasy_api_tests/services/issue_tls_server_certificate/issue_tls_server_certificates.py
@@ -8,8 +8,8 @@ from certeasy_api_tests.src.config import BASE_URL
 def issue_tls_server_certs():
     loaded_schema = file_reader(
         filename='../services/issue_tls_server_certificate/issue_tls_server_certificate_schema.json')
-    ISSUER_ID = create_issuer_from_spec()
+    CREATED_ISSUER = create_issuer_from_spec()
     VALID_BODY = generate_tls_server_data(loaded_schema)
-    issue_tls_server_cert = requests.post(url=f'{BASE_URL}/issuers/{ISSUER_ID[0]}/certificates/tls-server',
+    issue_tls_server_cert = requests.post(url=f'{BASE_URL}/issuers/{CREATED_ISSUER[0]}/certificates/tls-server',
                                           json=VALID_BODY)
-    return issue_tls_server_cert.json()["serial"], ISSUER_ID[0], VALID_BODY["name"], ISSUER_ID
+    return issue_tls_server_cert.json()["serial"], CREATED_ISSUER[0], VALID_BODY["name"], CREATED_ISSUER[1]["name"]
--- certeasy_api_tests/src/config.py
+++ certeasy_api_tests/src/config.py
@@ -2,5 +2,5 @@ import os
 
 DOCKER_IMAGE_PORT = '8080'
 # DOCKER_IMAGE_NAME = os.getenv('DOCKER_IMAGE_NAME')
-
+DOCKER_IMAGE_NAME = 'ghcr.io/emjunior258/certeasy:develop'
 BASE_URL = f"http://0.0.0.0:{DOCKER_IMAGE_PORT}/api"
