e3bcadd846da466e874be6c317f0dbfaea5bf262
root <root@pop-os.localdomain>
2024-04-29 09:43:34 +0200

    refactor: rename certeasy_api_tests to certeasy-api-tests

--- test-local.sh
+++ test-local.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pip install --upgrade pip
-pip install -r certeasy_api_tests/requirements.txt
-pytest-3 certeasy_api_tests/src/tests --junitxml=certeasy_api_tests/target/report.xml
+pip install -r certeasy-api-tests/requirements.txt
+pytest-3 certeasy-api-tests/src/tests --junitxml=certeasy-api-tests/target/report.xml
