b131ba7e23e4a082b07681f668069472d8c8561a
Ivan Vilanculo <vilanculoivan@gmail.com>
2024-07-16 18:26:57 +0200

    chore(front-end): update eslint configuration

--- certeasy-console-app/.eslintrc.cjs
+++ certeasy-console-app/.eslintrc.cjs
@@ -1,9 +1,18 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json', './tsconfig.app.json'],
+    tsconfigRootDir: __dirname,
+  },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
