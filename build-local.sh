48a5b278db44c6d724d054901369f457b7075786
Ivan Vilanculo <vilanculoivan@gmail.com>
2024-07-28 12:41:19 +0200

    chore(build): up local container build script to include console application

--- build-local.sh
+++ build-local.sh
@@ -1,16 +1,40 @@
 #!/bin/bash
-cd certeasy-core
-./mvnw clean install
-cd ../certeasy-bouncycastle
-./mvnw clean install
-cd ../certeasy-backend-app
-./mvnw package -Dnative -DskipTests -Dquarkus.native.container-build=true
-cd ../
-mkdir -p /tmp/certeasy-build
-cp Dockerfile /tmp/certeasy-build/
-cp startup.sh /tmp/certeasy-build/
-cp -r certeasy-console-app /tmp/certeasy-build/
-cp certeasy-backend-app/target/*-runner /tmp/certeasy-build/
-chmod +x /tmp/certeasy-build/*-runner
-cd /tmp/certeasy-build
-docker build -t certeasy:local .
+
+BUILD_DIR="/tmp/certeasy-build"
+
+function build_java_module() {
+    pushd $1
+    ./mvnw clean install
+    popd
+}
+
+function build_nodejs_module () {
+    pushd $1
+    pnpm install
+    pnpm build
+    cp -r ./dist "${BUILD_DIR}/$1"
+    popd
+}
+
+function build_backend_app () {
+    pushd certeasy-backend-app
+    ./mvnw package -Dnative -DskipTests -Dquarkus.native.container-build=true
+    cp ./target/*-runner "${BUILD_DIR}"
+    chmod +x ${BUILD_DIR}/*-runner
+    popd
+}
+
+function build_docker_image () {
+    cp Dockerfile "${BUILD_DIR}/"
+    cp startup.sh "${BUILD_DIR}/"
+    cp nginx.conf "${BUILD_DIR}/"
+    docker build -t certeasy:local ${BUILD_DIR}
+}
+
+# Ensure build dir
+mkdir -p "${BUILD_DIR}"
+build_nodejs_module "certeasy-console-app"
+build_java_module "certeasy-core"
+build_java_module "certeasy-bouncycastle"
+build_backend_app
+build_docker_image
