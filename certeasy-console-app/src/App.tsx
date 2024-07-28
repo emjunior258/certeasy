b2e699a757a6063ad30a725bd104d0c933c09273
Ivan Vilanculo <vilanculoivan@gmail.com>
2024-07-28 09:40:21 +0200

    chore: setup styled components

--- certeasy-console-app/src/App.tsx
+++ certeasy-console-app/src/App.tsx
@@ -1,12 +1,25 @@
-import { Suspense } from 'react'
+import { Fragment, Suspense } from 'react'
 import { useRoutes } from 'react-router-dom'
+import { createGlobalStyle } from 'styled-components'
+import { theme } from './theme'
+import 'reset-css'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: ${theme.fontFamily.body};
+    font-weight: ${theme.fontWeight.normal};
+  }
+`
 
 import routes from '~react-pages'
 
 export default function App() {
-    return (
+  return (
+    <Fragment>
+      <GlobalStyle />
       <Suspense fallback={<p>Loading...</p>}>
         {useRoutes(routes)}
       </Suspense>
-    )
-  }
+    </Fragment>
+  )
+}
