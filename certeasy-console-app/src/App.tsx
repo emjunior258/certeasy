bc88ba69febbc7480aae5eadea80a5bdcb7a406c
Ivan Vilanculo <vilanculoivan@gmail.com>
2024-11-10 11:07:11 +0200

    chore: improve theming

--- certeasy-console-app/src/App.tsx
+++ certeasy-console-app/src/App.tsx
@@ -1,14 +1,14 @@
 import { Suspense } from 'react'
 import { useRoutes } from 'react-router-dom'
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import { theme } from './theme'
 import 'reset-css'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 const GlobalStyle = createGlobalStyle`
   body {
-    font-family: ${theme.fontFamily.body};
-    font-weight: ${theme.fontWeight.normal};
+    font-family: ${({theme}) => theme.fontFamily.body};
+    font-weight: ${({theme}) => theme.fontWeight.normal};
   }
 `
 
@@ -16,15 +16,17 @@ import routes from '~react-pages'
 
 export default function App() {
   return (
-    <HelmetProvider>
-      <GlobalStyle />
-      <Helmet
-        title='The easiest certificate authority'
-        titleTemplate='%s - Certeasy'
-      />
-      <Suspense fallback={<p>Loading...</p>}>
-        {useRoutes(routes)}
-      </Suspense>
-    </HelmetProvider>
+    <ThemeProvider theme={theme}>
+      <HelmetProvider>
+        <GlobalStyle />
+        <Helmet
+          title='The easiest certificate authority'
+          titleTemplate='%s - Certeasy'
+        />
+        <Suspense fallback={<p>Loading...</p>}>
+          {useRoutes(routes)}
+        </Suspense>
+      </HelmetProvider>
+    </ThemeProvider>
   )
 }
